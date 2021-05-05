3d900144482f1a76b564549e7acdc2d9a9d95f80
comanoana <oanamariarodila95@yahoo.es>
2021-05-05 16:59:54 +0300

    working with portals

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import React from 'react';
 import AddUser from './components/Users/AddUser';
 import UsersList from './components/Users/UsersList';
-import { useState } from 'react';
+import { useState, Fragment } from 'react';
 
 function App() {
   const [usersList, setUsersList ] = useState([]);
 
 const addUserHandler = (uName, uAge) =>{
   setUsersList((prevUsersList) => {
-    return [...prevUsersList, {name:uName, age: uAge, id: Math.random().toString()}];
+    return [
+      ...prevUsersList, 
+      {name:uName, age: uAge, id: Math.random().toString() },
+    ]
 
   });
 };
 
   return (
-    <div>
+    <Fragment>
    <AddUser onAddUser={addUserHandler} />
    <UsersList users={usersList} /> 
-    </div>
+    </Fragment>
   );
 }
 
